5f23a32e99bc3d9cdbe2e8c3ed1bf4bffa6c16c6
Brattus <olemartinbratteberg@gmail.com>
2015-04-10 11:03:38 +0200

    OS Input

--- src/Harddisk.java
+++ src/Harddisk.java
@@ -1,25 +1,21 @@
-
 import java.io.*;
 
 
-public class Harddisk
-{
-    private String s;
-    public Harddisk()
-    {
-        String fileName = ("C:"+File.separator+"test"+File.separator+"odata.txt");
-        File dataFile = new File(fileName);
-        byte[] content = new byte[0];
-        {
-            try {FileInputStream dataInputStream = new FileInputStream(dataFile);
-                int bytesAvailable = dataInputStream.available();
-                content = new byte[bytesAvailable];
-                dataInputStream.read(content);
-                dataInputStream.close();}
-            catch (FileNotFoundException fnfe) {System.out.println("Couldn't find a file called " + fileName);}
-            catch (IOException ioe) {System.out.println("Couldn't read from a file called " + fileName);}
-            this.s = new String(content);
+public class Harddisk {
+    public static void main(String[] args) {
+        String text = "Hello world";
+        try {
+            File file = new File("example.txt");
+            BufferedWriter output = new BufferedWriter(new FileWriter(file));
+            output.write(text);
+            output.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
-    public String getpayload(){return s;}
+
+    public void createFile(){
+
+    }
+
 }
