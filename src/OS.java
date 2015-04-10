0dd17300a5033c6a9350024478f1fff0f20186e0
ivankana <ivankana@hotmail.com>
2015-04-10 11:04:12 +0200

    OS Input

--- src/OS.java
+++ src/OS.java
@@ -12,9 +12,14 @@ public class OS {
 
     //Scanner example
     Scanner input = new Scanner(System.in);
-    System.out.print("Enter something > ");
+        System.out.println("Instructions:");
+        System.out.println("- To read a file make sure to write read then the filename (example: read filename.txt)");
+        System.out.println("- To write a file make sure to write write then the filename (example: write filename.txt)");
+        System.out.println("- To seek a file make sure to write seek then the filename (example: seek filename.txt)");
+
+    System.out.print("What would you like to do? > ");
     String inputString = input.nextLine();
-    System.out.print("You entered : ");
+    System.out.print("You entered: ");
     System.out.println(inputString);
 
 
