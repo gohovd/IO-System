fa9cb14966a1cd4cc2be586e5e2074a81c659c10
Brattus <olemartinbratteberg@gmail.com>
2015-04-10 11:30:43 +0200

    IUpdate

--- src/Harddisk.java
+++ src/Harddisk.java
@@ -13,12 +13,24 @@ public class Harddisk {
                 BufferedWriter output = new BufferedWriter(new FileWriter(file,true));
                 for(Object s : text) {
                     String txt = s.toString();
-                    output.write(txt);
-                    System.out.print(txt);
+                    output.write(txt + " ");
+                    System.out.print(txt + " ");
                 }
                 output.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+
+    public void read(ArrayList filename){
+        String str = null;
+        try{
+            BufferedReader in = new BufferedReader(new FileReader(filename));
+            while(( str = in.readLine()) != null ){
+                System.out.println(str + "\n");
+            }
+        } catch (IOException e) {
+            System.out.println("something went wronk");
+        }
+    }
 }
